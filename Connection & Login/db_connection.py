8c52117bf0203f41825043f4352f11122eafe6e1
ullamas13 <llamasgonzalez.unai@gmail.com>
2026-05-16 17:13:41 +0200

    V4

--- Connection & Login/db_connection.py
+++ Connection & Login/db_connection.py
@@ -1,16 +1,37 @@
 import psycopg2
+from psycopg2 import OperationalError
 
 def get_connection():
-    """Estableix la connexió amb la base de dades PostgreSQL."""
+    """Estableix la connexió amb Alta Disponibilitat."""
+    
+    # Intentem servidor principal
     try:
+        print("Intentant connectar a node1...")
         connection = psycopg2.connect(
             host="node1",
             database="hospital",
             user="ua-admin", 
             password="admin123",
-            sslmode="require"  # Força el xifrat SSL amb el certificat autofirmat
+            sslmode="require"
         )
+        print("Connectat al node1 amb èxit.")
         return connection
-    except Exception as e:
-        print(f"Error de connexió: {e}")
-        return None
+        
+    except OperationalError:
+        print("node1 caigut. Activant protocol d'emergència cap a node2...")
+        
+        # Si el principal falla
+        try:
+            connection = psycopg2.connect(
+                host="node2", 
+                database="hospital",
+                user="ua-admin", 
+                password="admin123",
+                sslmode="require"
+            )
+            print("Connectat al node2 amb èxit.")
+            return connection
+            
+        except Exception as e:
+            print(f"Error crític: Els dos servidors estan caiguts. Detalls: {e}")
+            return None
